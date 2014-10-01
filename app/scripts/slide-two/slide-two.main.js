2f0dc61624d05542130a46a257c8ba4e00a12600
Paul Thibedeau <pthibedeau@gmail.com>
2014-10-01 11:36:47 -0400

    updates to animations, timing/durations

--- app/scripts/slide-two/slide-two.main.js
+++ app/scripts/slide-two/slide-two.main.js
@@ -9,7 +9,7 @@ define(['jquery', 'backbone', 'scrollmagic', ], function(){
 				var controller = new ScrollMagic();
 
 				var tween = TweenMax.from(".example-2 .icon", 1, {opacity : ".1", scale: 2.5, top : '-1150px'});
-				var tween2 = TweenMax.from(".example-2 .title", 1, { top : '-500px', delay : 0.3 });
+				var tween2 = TweenMax.from(".example-2 .title", 1, { top : '-500px', delay : 0.5 });
 				var tween3 = TweenMax.from(".example-2 summary", 2, { right : "-1000px", opacity : '.1', delay : .6 })
 				// build scene
 				var scene = new ScrollScene({ triggerElement: ".example-2" })
